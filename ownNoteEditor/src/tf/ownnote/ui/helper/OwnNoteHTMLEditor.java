9bc9bbb880b24e6b2044453748bdccf57992d1e5
ThomasDaheim <shopping@feuster.com>
2016-03-12 18:13:47 +0100

    Issue with images fixed
    Include images as base 64 data and not as link

--- ownNoteEditor/src/tf/ownnote/ui/helper/OwnNoteHTMLEditor.java
+++ ownNoteEditor/src/tf/ownnote/ui/helper/OwnNoteHTMLEditor.java
@@ -26,7 +26,7 @@
 package tf.ownnote.ui.helper;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -51,6 +51,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import org.apache.commons.io.FilenameUtils;
 import org.jsoup.Jsoup;
@@ -62,6 +63,15 @@ import tf.ownnote.ui.main.OwnNoteEditor;
  * @author Thomas Feuster <thomas@feuster.com>
  */
 public class OwnNoteHTMLEditor {
+    private static final String TOP_TOOLBAR = ".top-toolbar";
+    private static final String BOTTOM_TOOLBAR = ".bottom-toolbar";
+    private static final String WEB_VIEW = ".web-view";
+
+    private WebView mWebView;
+    private ToolBar mTopToolBar;
+    private ToolBar mBottomToolBar;    
+    
+    
     private HTMLEditor myHTMLEditor = null;
     
     // callback to OwnNoteEditor required for e.g. delete & rename
@@ -162,6 +172,10 @@ public class OwnNoteHTMLEditor {
     }
 
     private void initHTMLEditor() {
+        mWebView = (WebView) myHTMLEditor.lookup(WEB_VIEW);
+        mTopToolBar = (ToolBar) myHTMLEditor.lookup(TOP_TOOLBAR);
+        mBottomToolBar = (ToolBar) myHTMLEditor.lookup(BOTTOM_TOOLBAR);
+        
         // remove: foreground & background control
         hideNode(myHTMLEditor, ".html-editor-foreground", 1);
         hideNode(myHTMLEditor, ".html-editor-background", 1);
@@ -186,6 +200,23 @@ public class OwnNoteHTMLEditor {
             // TODO: fix 2 context menus - but how
             myHTMLEditor.setContextMenu(newContextMenu);
         }
+        
+        // TODO: add a drop handler, related to issue #31
+        // mWebView.setOnDragDropped((DragEvent event) -> {
+        //     Dragboard db = event.getDragboard();
+        //     boolean success = false;
+        //     if (db.hasString()) {
+        //         String filePath = null;
+        //         for (File file:db.getFiles()) {
+        //             filePath = file.getAbsolutePath();
+        //             System.out.println(filePath);
+        //         }
+        //         success = true;
+        //     }
+        //     event.setDropCompleted(success);
+        //     
+        //     event.consume();
+        // });
     }
     
     private static void hideNode(final Node startNode, final String lookupString, final int occurence) {
@@ -205,122 +236,114 @@ public class OwnNoteHTMLEditor {
     }
 
     private void addNoteEditorControls() {
-        Node node = myHTMLEditor.lookup(".top-toolbar");
-        if (node != null && node instanceof ToolBar) {
-            ToolBar toolbar = (ToolBar) node;
-            
-            // update edit - but only once
-            if (toolbar.lookup(".html-editor-insertlink") != null) {
-                return;
-            }
-
-            // copy styles from other buttons in toolbar
-            ObservableList<String> buttonStyles = null;
-            node = toolbar.lookup(".html-editor-cut");
-            if (node != null && node instanceof Button) {
-                buttonStyles = ((Button) node).getStyleClass();
-                // not the own button style, please
-                buttonStyles.removeAll("html-editor-cut");
-            }
-
-            // add button to insert link
-            ImageView graphic =
-                    new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/link.png"), 22, 22, true, true));
-            final Button insertLink = new Button("", graphic);
-            insertLink.getStyleClass().add("html-editor-insertlink");
-            if (buttonStyles != null) {
-                insertLink.getStyleClass().addAll(buttonStyles);
-            }
-            insertLink.setOnAction((ActionEvent t) -> {
-                LinkDialog linkDialog = new LinkDialog();
-                if (linkDialog.showAndWait()) {
-                    // dialog has been ended with OK - now check if values are fine
-                    if (!linkDialog.getLinkUrl().isEmpty() && !linkDialog.getLinkText().isEmpty()) {
-                        final String hrefString =
-                                "<a href=\"" +
-                                linkDialog.getLinkUrl().trim() +
-                                "\" title=\"" +
-                                linkDialog.getLinkTitle().trim() +
-                                "\" target=\"" +
-                                // decide between _self and _blank on the fly
-                                linkDialog.getWindowMode()+
-                                "\">" +
-                                linkDialog.getLinkText().trim() + "</a>";
-                        myHTMLEditor.setHtmlText(myHTMLEditor.getHtmlText() + hrefString);
-                    }
-                }
-            });
-            toolbar.getItems().add(insertLink);
-
-            // add button to insert image
-            graphic = 
-                    new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/insertimage.gif"), 22, 22, true, true));
-            final Button insertImage = new Button("", graphic);
-            insertImage.getStyleClass().add("html-editor-insertimage");
-            if (buttonStyles != null) {
-                insertImage.getStyleClass().addAll(buttonStyles);
-            }
-            insertImage.setOnAction((ActionEvent arg0) -> {
-                final List<String> extFilter = Arrays.asList("*.jpg", "*.png", "*.gif");
-                final List<String> extValues = Arrays.asList("jpg", "png", "gif");
-
-                final FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Embed an image");
-                fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Pictures", extFilter));
-                final File selectedFile = fileChooser.showOpenDialog(null);
-
-                if (selectedFile != null) {
-                    if (extValues.contains(FilenameUtils.getExtension(selectedFile.getName()).toLowerCase())) {
-                        try {
-                            // we really have selected a picture - now add it
-                            myHTMLEditor.setHtmlText(
-                                    myHTMLEditor.getHtmlText() + "<img src='" + selectedFile.toURI().toURL().toExternalForm() +"'>");
-                        } catch (MalformedURLException ex) {
-                            Logger.getLogger(OwnNoteEditor.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }                        
-                }
-            });
-            toolbar.getItems().add(insertImage);
-            
-            // add separator
-            toolbar.getItems().add(new Separator());
-
-            // Issue #12 - add print button
-            // https://stackoverflow.com/questions/28847757/how-to-display-print-dialog-in-java-fx-and-print-node
-            graphic =
-                    new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/print.png"), 22, 22, true, true));
-            final Button printNote = new Button("", graphic);
-            printNote.getStyleClass().add("html-editor-print");
-            if (buttonStyles != null) {
-                printNote.getStyleClass().addAll(buttonStyles);
-            }
-            printNote.setOnAction((ActionEvent arg0) -> {
-                PrinterJob job = PrinterJob.createPrinterJob();
-                if (job != null && job.showPrintDialog(myHTMLEditor.getScene().getWindow())){
-                    myHTMLEditor.print(job);
-                    job.endJob();
-                }
-            });
-            toolbar.getItems().add(printNote);
-
-            // add separator
-            toolbar.getItems().add(new Separator());
-
-            // add save button
-            graphic =
-                    new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/save.png"), 22, 22, true, true));
-            final Button saveNote = new Button("", graphic);
-            saveNote.getStyleClass().add("html-editor-print");
-            if (buttonStyles != null) {
-                saveNote.getStyleClass().addAll(buttonStyles);
-            }
-            saveNote.setOnAction((ActionEvent arg0) -> {
-                saveNote();
-            });
-            toolbar.getItems().add(saveNote);
+        // update edit - but only once
+        if (mTopToolBar.lookup(".html-editor-insertlink") != null) {
+            return;
         }
+
+        // copy styles from other buttons in toolbar
+        ObservableList<String> buttonStyles = null;
+        Node node = mTopToolBar.lookup(".html-editor-cut");
+        if (node != null && node instanceof Button) {
+            buttonStyles = ((Button) node).getStyleClass();
+            // not the own button style, please
+            buttonStyles.removeAll("html-editor-cut");
+        }
+
+        // add button to insert link
+        ImageView graphic =
+                new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/link.png"), 22, 22, true, true));
+        final Button insertLink = new Button("", graphic);
+        insertLink.getStyleClass().add("html-editor-insertlink");
+        if (buttonStyles != null) {
+            insertLink.getStyleClass().addAll(buttonStyles);
+        }
+        insertLink.setOnAction((ActionEvent t) -> {
+            LinkDialog linkDialog = new LinkDialog();
+            if (linkDialog.showAndWait()) {
+                // dialog has been ended with OK - now check if values are fine
+                if (!linkDialog.getLinkUrl().isEmpty() && !linkDialog.getLinkText().isEmpty()) {
+                    final String hrefString =
+                            "<a href=\"" +
+                            linkDialog.getLinkUrl().trim() +
+                            "\" title=\"" +
+                            linkDialog.getLinkTitle().trim() +
+                            "\" target=\"" +
+                            // decide between _self and _blank on the fly
+                            linkDialog.getWindowMode()+
+                            "\">" +
+                            linkDialog.getLinkText().trim() + "</a>";
+                    myHTMLEditor.setHtmlText(myHTMLEditor.getHtmlText() + hrefString);
+                }
+            }
+        });
+        mTopToolBar.getItems().add(insertLink);
+
+        // add button to insert image
+        graphic = 
+                new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/insertimage.gif"), 22, 22, true, true));
+        final Button insertImage = new Button("", graphic);
+        insertImage.getStyleClass().add("html-editor-insertimage");
+        if (buttonStyles != null) {
+            insertImage.getStyleClass().addAll(buttonStyles);
+        }
+        insertImage.setOnAction((ActionEvent arg0) -> {
+            final List<String> extFilter = Arrays.asList("*.jpg", "*.png", "*.gif");
+            final List<String> extValues = Arrays.asList("jpg", "png", "gif");
+
+            final FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Embed an image");
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Pictures", extFilter));
+            final File selectedFile = fileChooser.showOpenDialog(null);
+
+            if (selectedFile != null) {
+                if (extValues.contains(FilenameUtils.getExtension(selectedFile.getName()).toLowerCase())) {
+                    // we really have selected a picture - now add it
+                    // issue #31: we shouldn't add the link but the image data instead!
+                    // see https://github.com/dipu-bd/CustomControlFX/blob/master/CustomHTMLEditor/src/org/sandsoft/components/htmleditor/CustomHTMLEditor.java
+                    importDataFile(selectedFile);
+                }                        
+            }
+        });
+        mTopToolBar.getItems().add(insertImage);
+
+        // add separator
+        mTopToolBar.getItems().add(new Separator());
+
+        // Issue #12 - add print button
+        // https://stackoverflow.com/questions/28847757/how-to-display-print-dialog-in-java-fx-and-print-node
+        graphic =
+                new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/print.png"), 22, 22, true, true));
+        final Button printNote = new Button("", graphic);
+        printNote.getStyleClass().add("html-editor-print");
+        if (buttonStyles != null) {
+            printNote.getStyleClass().addAll(buttonStyles);
+        }
+        printNote.setOnAction((ActionEvent arg0) -> {
+            PrinterJob job = PrinterJob.createPrinterJob();
+            if (job != null && job.showPrintDialog(myHTMLEditor.getScene().getWindow())){
+                myHTMLEditor.print(job);
+                job.endJob();
+            }
+        });
+        mTopToolBar.getItems().add(printNote);
+
+        // add separator
+        mTopToolBar.getItems().add(new Separator());
+
+        // add save button
+        graphic =
+                new ImageView(new Image(OwnNoteEditor.class.getResourceAsStream("/tf/ownnote/ui/css/save.png"), 22, 22, true, true));
+        final Button saveNote = new Button("", graphic);
+        saveNote.getStyleClass().add("html-editor-print");
+        if (buttonStyles != null) {
+            saveNote.getStyleClass().addAll(buttonStyles);
+        }
+        saveNote.setOnAction((ActionEvent arg0) -> {
+            saveNote();
+        });
+        mTopToolBar.getItems().add(saveNote);
     }
 
     @SuppressWarnings("unchecked")
@@ -333,6 +356,79 @@ public class OwnNoteHTMLEditor {
                 curNote.getNoteName(), 
                 getNoteText())) {
             hasBeenSaved();
+        }
+    }
+    
+    /**
+     * Inserts HTML data after the current cursor. If anything is selected, they
+     * get replaced with new HTML data.
+     *
+     * @param html HTML data to insert.
+     */
+    private void insertHtmlAfterCursor(String html) {
+        //replace invalid chars
+        html = html.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\r", "\\r")
+                .replace("\n", "\\n");
+        //get script
+        String script = String.format(
+                "(function(html) {"
+                + "  var sel, range;"
+                + "  if (window.getSelection) {"
+                + "    sel = window.getSelection();"
+                + "    if (sel.getRangeAt && sel.rangeCount) {"
+                + "      range = sel.getRangeAt(0);"
+                + "      range.deleteContents();"
+                + "      var el = document.createElement(\"div\");"
+                + "      el.innerHTML = html;"
+                + "      var frag = document.createDocumentFragment(),"
+                + "        node, lastNode;"
+                + "      while ((node = el.firstChild)) {"
+                + "        lastNode = frag.appendChild(node);"
+                + "      }"
+                + "      range.insertNode(frag);"
+                + "      if (lastNode) {"
+                + "        range = range.cloneRange();"
+                + "        range.setStartAfter(lastNode);"
+                + "        range.collapse(true);"
+                + "        sel.removeAllRanges();"
+                + "        sel.addRange(range);"
+                + "      }"
+                + "    }"
+                + "  }"
+                + "  else if (document.selection && "
+                + "           document.selection.type != \"Control\") {"
+                + "    document.selection.createRange().pasteHTML(html);"
+                + "  }"
+                + "})(\"%s\");", html);
+        //execute script
+        mWebView.getEngine().executeScript(script);
+    }
+    
+    /**
+     * Imports an image file.
+     *
+     * @param file Image file.
+     */
+    private void importDataFile(File file) {
+        try {
+            //check if file is too big
+            if (file.length() > 1024 * 1024) {
+                throw new VerifyError("File is too big.");
+            }
+            //get mime type of the file
+            String type = java.nio.file.Files.probeContentType(file.toPath());
+            //get html content
+            byte[] data = org.apache.commons.io.FileUtils.readFileToByteArray(file);
+            String base64data = java.util.Base64.getEncoder().encodeToString(data);
+            String htmlData = String.format(
+                    "<img src='data:%s;base64,%s' type='%s' />",
+                    type, base64data, type);
+            //insert html
+            insertHtmlAfterCursor(htmlData);
+        } catch (IOException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
         }
     }
     
