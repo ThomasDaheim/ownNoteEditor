efc87f65a503d23089fab8b9e9b628deeefa55fd
Thomas Feuster <shopping@feuster.com>
2023-08-27 17:38:18 +0200

    Show edit notes task at beginning of tasks list

--- src/main/java/tf/ownnote/ui/tasks/TaskList.java
+++ src/main/java/tf/ownnote/ui/tasks/TaskList.java
@@ -195,7 +195,9 @@ public class TaskList {
         // TFE, 20210301: highlight tasks for current note
         myEditor.currentNoteProperty().addListener((ov, oldNote, newNote) -> {
             // set styles accordingly
-            myTaskList.refresh();
+            // TFE, 20230827: we want to have edited note always on top of the list!
+//            myTaskList.refresh();
+            initListData();
         });
     }
     
@@ -240,10 +242,15 @@ public class TaskList {
                 return 1;
             }
             // compare note names
-            int result = o1.getNote().getNoteName().compareTo(o2.getNote().getNoteName());
+            int result = o1.getNote().getNoteFileName().compareTo(o2.getNote().getNoteFileName());
+            // TFE, 20230827: we want to have edited note always on top of the list!
             if (result == 0) {
                 // sort by textpos for same note
                 result = o1.getTextPos() - o2.getTextPos();
+            } else if (o1.getNote().equals(myEditor.currentNoteProperty().get())) {
+                result = -1;
+            } else if (o2.getNote().equals(myEditor.currentNoteProperty().get())) {
+                result = 1;
             }
             return result;
         });
